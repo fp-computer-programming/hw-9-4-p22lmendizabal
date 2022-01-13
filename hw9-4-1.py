3de808df67e1619017c911fe13ea43d11808cafb
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-01-13 10:20:11 -0500

    add

---
+++ hw9-4-1.py
@@ -0,0 +1,10 @@
+#author: LM (AMDG) 1/14/22
+
+def smash(lst):
+    new_sentence = ""
+    for index, value in enumerate(lst):
+        index
+        print(new_sentence)
+
+smash(['my', 'name', 'is', 'luis'])
+        
